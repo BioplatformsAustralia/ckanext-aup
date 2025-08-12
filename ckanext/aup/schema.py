f4553f680370e6f2a975d706e301681c61cf1d83
Mark Tearle <mark.tearle@qcif.edu.au>
2025-08-12 15:47:58 +0800

    Remove misleading comments

--- ckanext/aup/schema.py
+++ ckanext/aup/schema.py
@@ -42,8 +42,6 @@ def aup_update():
 
 def aup_clear():
     #  user_id (string)
-    #  package_id (string)
-    #  valid_until (iso date string) - (optional)
     not_empty = tk.get_validator("not_empty")
     ignore_missing = tk.get_validator("ignore_missing")
     user_id_or_name_exists = tk.get_validator("user_id_or_name_exists")
