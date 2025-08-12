264b0d037960223e99e198bcb7718048f5b7d131
Mark Tearle <mark.tearle@qcif.edu.au>
2025-08-12 15:46:07 +0800

    Add auth functions

---
+++ ckanext/aup/auth.py
@@ -0,0 +1,70 @@
+# coding: utf8
+import ckan.plugins.toolkit as tk
+import ckan.authz as authz
+from ckan.common import g, _
+from ckanext.toolbelt.decorators import Collector
+
+from logging import getLogger
+
+log = getLogger(__name__)
+
+action, get_auth_functions = Collector().split()
+
+def _only_registered_user():
+    if not authz.auth_is_loggedin_user():
+        return {'success': False, 'msg': _('User is not logged in')}
+    return {'success': True}
+
+
+def _only_admin_user_for_field(data_dict, field):
+    """ Only allowed to sysadmins or organization admins """
+    if not authz.auth_is_loggedin_user():
+        return {'success': False, 'msg': _('User is not logged in')}
+
+    if not g.userobj:
+        return {'success': False}
+
+    if authz.is_sysadmin(g.user):
+        return {'success': True}
+
+    if data_dict.get(field, None) and not authz.is_sysadmin(g.user):
+        return {'success': False, 'msg': _('User must be a sysadmin')}
+
+    # fall through
+    return {}
+
+@action
+def aup_update(context, data_dict):
+    tk.check_access("aup_update", context, data_dict)
+
+    # retreive user obj
+    # only update non-self user if admin
+    # only to provided revision if admin
+
+    return _only_registered_user() | _only_admin_user_for_field(data_dict, "revision") |  _only_admin_user_for_field(data_dict, "user_id")
+
+
+@action
+def aup_revision(context, data_dict):
+    tk.check_access("aup_revision", context, data_dict)
+
+    # retreive user obj
+    # only non-self user if admin
+
+    return _only_registered_user() | _only_admin_user_for_field(data_dict, "user_id")
+
+
+@action
+def aup_clear(context, data_dict):
+    tk.check_access("aup_clear", context, data_dict)
+
+    # retreive user obj
+    # only update non-self user if admin
+
+    return _only_registered_user() | _only_admin_user_for_field(data_dict, "user_id")
+
+
+@action
+@tk.auth_allow_anonymous_access
+def aup_published(context, data_dict):
+    return {"success": True}
--- ckanext/aup/plugin.py
+++ ckanext/aup/plugin.py
@@ -5,12 +5,17 @@ import ckanext.aup.action as action
 import ckanext.aup.views as views
 import ckanext.aup.interface as interface
 import ckanext.aup.helpers as helpers
+import ckanext.aup.auth as auth
 
+from logging import getLogger
+
+logger = getLogger(__name__)
 
 class AupPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.ITemplateHelpers)
     plugins.implements(plugins.IActions)
+    plugins.implements(plugins.IAuthFunctions)
     plugins.implements(plugins.IBlueprint)
     plugins.implements(interface.IAcceptableUse, inherit=True)
     
@@ -48,6 +53,11 @@ class AupPlugin(plugins.SingletonPlugin):
 
     def get_actions(self):
         return action.get_actions()
+
+    # IAuthFunctions
+
+    def get_auth_functions(self):
+        return auth.get_auth_functions()
 
     # IBlueprint
 
