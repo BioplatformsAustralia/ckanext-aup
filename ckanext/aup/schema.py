dec72e8063c72e20ddde54af28b44f251aed9e2b
Mark Tearle <mark.tearle@qcif.edu.au>
2025-09-18 12:19:42 +0800

    Fix test for aup_update()
    Relates BioplatformsAustralia/ckanext-aup#4

--- ckanext/aup/schema.py
+++ ckanext/aup/schema.py
@@ -4,6 +4,7 @@ def aup_changed():
     #  user_id (string)
     not_empty = tk.get_validator("not_empty")
     ignore_missing = tk.get_validator("ignore_missing")
+    unicode_safe = tk.get_validator("unicode_safe")
     user_id_or_name_exists = tk.get_validator("user_id_or_name_exists")
     convert_user_name_or_id_to_id = tk.get_validator("convert_user_name_or_id_to_id")
 
@@ -21,6 +22,7 @@ def aup_revision():
     #  user_id (string)
     not_empty = tk.get_validator("not_empty")
     ignore_missing = tk.get_validator("ignore_missing")
+    unicode_safe = tk.get_validator("unicode_safe")
     user_id_or_name_exists = tk.get_validator("user_id_or_name_exists")
     convert_user_name_or_id_to_id = tk.get_validator("convert_user_name_or_id_to_id")
 
@@ -39,6 +41,7 @@ def aup_update():
     #  revision (string)
     not_empty = tk.get_validator("not_empty")
     ignore_missing = tk.get_validator("ignore_missing")
+    unicode_safe = tk.get_validator("unicode_safe")
     user_id_or_name_exists = tk.get_validator("user_id_or_name_exists")
     convert_user_name_or_id_to_id = tk.get_validator("convert_user_name_or_id_to_id")
 
@@ -60,6 +63,7 @@ def aup_clear():
     #  user_id (string)
     not_empty = tk.get_validator("not_empty")
     ignore_missing = tk.get_validator("ignore_missing")
+    unicode_safe = tk.get_validator("unicode_safe")
     user_id_or_name_exists = tk.get_validator("user_id_or_name_exists")
     convert_user_name_or_id_to_id = tk.get_validator("convert_user_name_or_id_to_id")
 
