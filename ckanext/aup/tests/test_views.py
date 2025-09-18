16eace4763557248e59df053a80aa52479beb33a
Mark Tearle <mark.tearle@qcif.edu.au>
2025-09-18 14:43:56 +0800

    Test AUP published view
    Relates BioplatformsAustralia/ckanext-aup#10

--- ckanext/aup/tests/test_views.py
+++ ckanext/aup/tests/test_views.py
@@ -12,3 +12,11 @@ def test_aup_rejected(app, reset_db):
     assert resp.status_code == 200
     # look for phrase from rejection template
     assert (resp.body.find('has been rejected by the user') >= 0)
+
+@pytest.mark.ckan_config("ckan.plugins", "aup")
+@pytest.mark.usefixtures("with_plugins")
+def test_aup_published(app, reset_db):
+    resp = app.get(tk.h.url_for("aup.aup_published"))
+    assert resp.status_code == 200
+    # look for phrase from rejection template
+    assert (resp.body.find('placeholder for where you should describe the Acceptable Use Policy') >= 0)
