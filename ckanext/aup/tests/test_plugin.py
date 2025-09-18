3dc5d0ffbb66c2c4e71ba4fe6877b90be7a6a530
Mark Tearle <mark.tearle@qcif.edu.au>
2025-09-18 10:21:40 +0800

    Fix test for aup_changed()
    Relates BioplatformsAustralia/ckanext-aup#4

--- ckanext/aup/tests/test_plugin.py
+++ ckanext/aup/tests/test_plugin.py
@@ -71,9 +71,11 @@ class TestAUPPlugin(object):
     @pytest.mark.usefixtures("clean_db")
     def test_aup_changed(self):
         user = factories.User(
-        #    plugin_extras={
-        #        'acceptable_use_policy_revision': '41'
-        #    }
+            plugin_extras={
+                'aup': {
+                    'acceptable_use_policy_revision': '41'
+                }
+            }
         )
 
         context = {
@@ -88,9 +90,11 @@ class TestAUPPlugin(object):
         assert(result == True)
 
         user2 = factories.User(
-        #    plugin_extras={
-        #        'acceptable_use_policy_revision': '42'
-        #    }
+            plugin_extras={
+                'aup': {
+                    'acceptable_use_policy_revision': '42'
+                }
+            }
         )
 
         context = {
